102a48dab733ad990a5e9c1da94cd9d9e2122020
Brad Abrahams <email@bradabrahams.co.uk>
2015-01-17 19:48:35 +0000

    Retrieve Flickr API using JSONP
    Add photos to main React UI state.

--- assets/js/site/components/ui.jsx
+++ assets/js/site/components/ui.jsx
@@ -4,20 +4,71 @@ App.UI = React.createClass({
 
     mixins: [ReactMiniRouter.RouterMixin],
 
+    /**
+     * Define #! URL routes.
+     */
+
     routes: {
         '/': 'feed',
         '/photo/:id': 'detail'
+    },
+
+    getInitialState: function() {
+        return {
+            photos: []
+        };
+    },
+
+    /**
+    * Load Flickr photos using JSONP and add them to the state.
+    */
+
+    loadPhotos: function () {
+        $.ajax({
+            url: "https://api.flickr.com/services/feeds/photos_public.gne?tags=potato&tagmode=all&format=json&jsoncallback=?",
+            type: "GET",
+            cache: true,
+            dataType: 'jsonp',
+            success: function(data) {
+                var photos;
+
+                if (typeof data.items !== 'undefined') {
+                    photos = data.items;
+                } else {
+                    photos = [];
+                }
+
+                // add retrieved Flickr photos to the state
+                this.setState({
+                    photos: photos
+                });
+            }.bind(this)
+        });
+    },
+
+    componentDidMount: function() {
+        this.loadPhotos();
     },
 
     render: function() {
         return this.renderCurrentRoute();
     },
 
+    /**
+     * Main photo feed.
+     * Found in ./components
+     */
+
     feed: function() {
         return (
             <App.Feed />
         );
     },
+
+    /**
+     * Detail view.
+     * Found in ./components
+     */
 
     detail: function(id) {
         return (
--- assets/js/site/render.jsx
+++ assets/js/site/render.jsx
@@ -1,3 +1,7 @@
+/**
+ * Render the main container component (App.UI).
+ */
+
 React.render(
     <App.UI />,
     document.getElementById('app')
